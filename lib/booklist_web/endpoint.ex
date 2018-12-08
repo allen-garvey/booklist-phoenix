871f78f0a708403257258c369d30f24791315ffa
Allen Garvey <allen.garvey@gmail.com>
2018-12-07 23:58:33 -0500

    Remove unused sockets

--- lib/booklist_web/endpoint.ex
+++ lib/booklist_web/endpoint.ex
@@ -1,10 +1,6 @@
 defmodule BooklistWeb.Endpoint do
   use Phoenix.Endpoint, otp_app: :booklist
 
-  socket "/socket", BooklistWeb.UserSocket,
-    websocket: true,
-    longpoll: false
-
   # Serve at "/" the static files from "priv/static" directory.
   #
   # You should set gzip to true if you are running phx.digest
