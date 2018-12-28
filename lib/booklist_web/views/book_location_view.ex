1f705fb4bdd53b0a27798f551ac846042c9a40ae
Allen Garvey <allen.garvey@gmail.com>
2018-12-27 23:24:44 -0500

    Change book locations to use shared index page template

--- lib/booklist_web/views/book_location_view.ex
+++ lib/booklist_web/views/book_location_view.ex
@@ -1,3 +1,7 @@
 defmodule BooklistWeb.BookLocationView do
   use BooklistWeb, :view
+
+  def to_s(book_location) do
+  	BooklistWeb.BookView.to_s(book_location.book) <> "—" <> BooklistWeb.LocationView.to_s(book_location.location)
+  end
 end
