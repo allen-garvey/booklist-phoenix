74d9e5c289272fc35267bdd0955a8ea814d20616
Allen Garvey <allen.garvey@gmail.com>
2018-12-27 23:23:38 -0500

    Edit location resource to have both full and shortened to string functions

--- lib/booklist_web/views/location_view.ex
+++ lib/booklist_web/views/location_view.ex
@@ -1,7 +1,11 @@
 defmodule BooklistWeb.LocationView do
   use BooklistWeb, :view
 
-  def to_s(location) do
+  def to_s_full(location) do
   	BooklistWeb.LibraryView.to_s(location.library) <> "—" <> location.name
+  end
+
+  def to_s(location) do
+  	location.name
   end
 end
