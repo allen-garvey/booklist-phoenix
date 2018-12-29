4f042f601940f4cea335d7f12e8b3c56bf670ff2
Allen Garvey <allen.garvey@gmail.com>
2018-12-28 23:36:01 -0500

    Change rating resource to use shared index page template

--- lib/booklist_web/views/rating_view.ex
+++ lib/booklist_web/views/rating_view.ex
@@ -1,3 +1,7 @@
 defmodule BooklistWeb.RatingView do
   use BooklistWeb, :view
+
+  def to_s(rating) do
+  	BooklistWeb.BookView.to_s(rating.book) <> "—" <> BooklistWeb.SharedView.us_formatted_date(rating.date_scored)
+  end
 end
