105c8a7b7c4486e4973d36047a3120d99d822e36
Allen Garvey <allen.garvey@gmail.com>
2018-12-31 19:39:49 -0500

    Add related fields to book location new and edit routes

--- lib/booklist_web/controllers/book_location_controller.ex
+++ lib/booklist_web/controllers/book_location_controller.ex
@@ -4,6 +4,13 @@ defmodule BooklistWeb.BookLocationController do
   alias Booklist.Admin
   alias Booklist.Admin.BookLocation
 
+  def related_fields() do
+    [
+      books: Admin.list_books() |> BooklistWeb.BookView.map_for_form,
+      locations: Admin.list_locations() |> BooklistWeb.LocationView.map_for_form,
+    ]
+  end
+
   def index(conn, _params) do
     book_locations = Admin.list_book_locations()
     render(conn, "index.html", book_locations: book_locations)
@@ -11,7 +18,7 @@ defmodule BooklistWeb.BookLocationController do
 
   def new(conn, _params) do
     changeset = Admin.change_book_location(%BookLocation{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", [changeset: changeset] ++ related_fields())
   end
 
   def create(conn, %{"book_location" => book_location_params}) do
@@ -22,7 +29,7 @@ defmodule BooklistWeb.BookLocationController do
         |> redirect(to: Routes.book_location_path(conn, :show, book_location))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", [changeset: changeset] ++ related_fields())
     end
   end
 
@@ -34,7 +41,7 @@ defmodule BooklistWeb.BookLocationController do
   def edit(conn, %{"id" => id}) do
     book_location = Admin.get_book_location!(id)
     changeset = Admin.change_book_location(book_location)
-    render(conn, "edit.html", book_location: book_location, changeset: changeset)
+    render(conn, "edit.html", [book_location: book_location, changeset: changeset] ++ related_fields())
   end
 
   def update(conn, %{"id" => id, "book_location" => book_location_params}) do
@@ -47,7 +54,7 @@ defmodule BooklistWeb.BookLocationController do
         |> redirect(to: Routes.book_location_path(conn, :show, book_location))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", book_location: book_location, changeset: changeset)
+        render(conn, "edit.html", [book_location: book_location, changeset: changeset] ++ related_fields())
     end
   end
 
