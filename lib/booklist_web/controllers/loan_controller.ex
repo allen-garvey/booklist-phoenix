5b95ac35b62a8d831932605021daacaf88e832e7
Allen Garvey <allen.garvey@gmail.com>
2018-12-31 19:31:18 -0500

    Add related fields to loan controller for new and edit routes

--- lib/booklist_web/controllers/loan_controller.ex
+++ lib/booklist_web/controllers/loan_controller.ex
@@ -4,6 +4,12 @@ defmodule BooklistWeb.LoanController do
   alias Booklist.Admin
   alias Booklist.Admin.Loan
 
+  def related_fields() do
+    [
+      libraries: Admin.list_libraries() |> BooklistWeb.LibraryView.map_for_form,
+    ]
+  end
+
   def index(conn, _params) do
     loans = Admin.list_loans()
     render(conn, "index.html", loans: loans)
@@ -11,7 +17,7 @@ defmodule BooklistWeb.LoanController do
 
   def new(conn, _params) do
     changeset = Admin.change_loan(%Loan{})
-    render(conn, "new.html", changeset: changeset)
+    render(conn, "new.html", [changeset: changeset] ++ related_fields())
   end
 
   def create(conn, %{"loan" => loan_params}) do
@@ -22,7 +28,7 @@ defmodule BooklistWeb.LoanController do
         |> redirect(to: Routes.loan_path(conn, :show, loan))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "new.html", changeset: changeset)
+        render(conn, "new.html", [changeset: changeset] ++ related_fields())
     end
   end
 
@@ -34,7 +40,7 @@ defmodule BooklistWeb.LoanController do
   def edit(conn, %{"id" => id}) do
     loan = Admin.get_loan!(id)
     changeset = Admin.change_loan(loan)
-    render(conn, "edit.html", loan: loan, changeset: changeset)
+    render(conn, "edit.html", [loan: loan, changeset: changeset] ++ related_fields())
   end
 
   def update(conn, %{"id" => id, "loan" => loan_params}) do
@@ -47,7 +53,7 @@ defmodule BooklistWeb.LoanController do
         |> redirect(to: Routes.loan_path(conn, :show, loan))
 
       {:error, %Ecto.Changeset{} = changeset} ->
-        render(conn, "edit.html", loan: loan, changeset: changeset)
+        render(conn, "edit.html", [loan: loan, changeset: changeset] ++ related_fields())
     end
   end
 
