84251e11e8901ff9a8251a03cae65c1633ee9876
Allen Garvey <allen.garvey@gmail.com>
2019-01-07 15:47:47 -0500

    Edit book index links on homepage and navigation to use active and read params

--- lib/booklist_web/views/book_view.ex
+++ lib/booklist_web/views/book_view.ex
@@ -11,4 +11,32 @@ defmodule BooklistWeb.BookView do
   def map_for_form(books) do
     Enum.map(books, &{to_s(&1), &1.id})
   end
+
+  @doc """
+  Path for active books 
+  """
+  def active_book_path(conn) do
+    BooklistWeb.Router.Helpers.book_path(conn, :index, active: "true")
+  end
+
+  @doc """
+  Path for active books that are unread (i.e. have no ratings)
+  """
+  def active_unread_book_path(conn) do
+    BooklistWeb.Router.Helpers.book_path(conn, :index, active: "true", read: "false")
+  end
+
+  @doc """
+  Path for inactive books that are unread (i.e. have no ratings)
+  """
+  def inactive_unread_book_path(conn) do
+    BooklistWeb.Router.Helpers.book_path(conn, :index, active: "false", read: "false")
+  end
+
+  @doc """
+  Path for inactive books that have been read (i.e. have ratings)
+  """
+  def inactive_read_book_path(conn) do
+    BooklistWeb.Router.Helpers.book_path(conn, :index, active: "false", read: "true")
+  end
 end
