af4c1a475c3b2997350ec520a4ccb44649befb4b
Allen Garvey <allen.garvey@gmail.com>
2019-01-13 23:38:45 -0500

    Fix error on reports show page when year is non integer parameter

--- lib/booklist_web/controllers/reports_controller.ex
+++ lib/booklist_web/controllers/reports_controller.ex
@@ -3,9 +3,7 @@ defmodule BooklistWeb.ReportsController do
 
   alias Booklist.Reports
 
-
-  def show(conn, %{"year" => year_raw}) do
-    year = String.to_integer(year_raw)
+  def report_for_year(conn, year) when is_integer(year) do
     rating_stats = Reports.get_rating_statistics(year)
     lowest_rating = Reports.get_lowest_rating(year)
     top_ratings = Reports.get_top_ratings(year, 10)
@@ -14,8 +12,14 @@ defmodule BooklistWeb.ReportsController do
       _             -> nil
     end
 
-
     render(conn, "show.html", year: year, rating_stats: rating_stats, lowest_rating: lowest_rating, highest_rating: highest_rating, top_ratings: top_ratings)
+  end
+
+  def show(conn, %{"year" => year_raw}) do
+    case Integer.parse(year_raw) do
+      {year, _} -> report_for_year(conn, year)
+      _         -> redirect(conn, to: BooklistWeb.ReportsView.reports_for_current_year_path(conn))
+    end
   end
 
   
