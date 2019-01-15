7c22fd3ad7d1c5717d2c175700abdbb53fae0830
Allen Garvey <allen.garvey@gmail.com>
2019-01-14 22:09:18 -0500

    Add initial query to get count of ratings by week number

--- lib/booklist/admin/reports.ex
+++ lib/booklist/admin/reports.ex
@@ -28,6 +28,16 @@ defmodule Booklist.Reports do
   	from(r in Rating, where: fragment("EXTRACT(year FROM ?)", r.date_scored) == ^year, select: [count(r.id), avg(r.score)])
       |> Repo.one
   end
+  @doc """
+  Gets the week number and number of ratings (books read) in that week for the given year
+  Can't limit to current week, because Elixir has not yet implemented function to get week of the year
+  How to group by using alias based on:
+  https://angelika.me/2016/09/10/how-to-order-by-the-result-of-select-count-in-ecto/
+  """
+  def get_ratings_count_by_week(year) do
+	from(r in Rating, where: fragment("EXTRACT(year FROM ?)", r.date_scored) == ^year, group_by: fragment("week"), select: %{week: fragment("extract(week FROM ?) AS week", r.date_scored), count: count(r.id)}, order_by: [fragment("week")])
+  	  |> Repo.all	
+  end
 
 
 end
