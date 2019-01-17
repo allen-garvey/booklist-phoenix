7b3cd6d8b28ab2dbb3d342d554be42b21f3069ec
Allen Garvey <allen.garvey@gmail.com>
2019-01-17 12:39:14 -0500

    Add mix task to migrate genres from legacy repo

---
+++ lib/mix/tasks/teamster/teamster.ex
@@ -0,0 +1,31 @@
+defmodule Mix.Tasks.Teamster do
+  use Mix.Task
+
+  import Ecto.Query, warn: false
+
+  alias Booklist.Repo
+  alias Booklist.RepoLegacy
+
+  alias Teamster.Models.LegacyGenre
+
+  @moduledoc """
+  Mix tasks to migrate the book_list rails database data to the booklist phoenix application 
+  """
+
+  @shortdoc "Migrates data from book_list rails to current booklist phoenix application"
+  def run(_) do
+    #start app so repo is available
+    Mix.Task.run "app.start", []
+
+    genres = RepoLegacy.all(from(LegacyGenre, order_by: [:id]))
+    migrate_legacy_models(genres, LegacyGenre)
+  end
+
+
+  defp migrate_legacy_models(legacy_models, module_name) when is_atom(module_name) do
+  	for legacy_model <- legacy_models do
+		Kernel.apply(module_name, :migrate, [legacy_model])
+			|> Repo.insert!
+    end
+  end
+end
